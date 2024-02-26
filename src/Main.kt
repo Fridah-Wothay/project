8a0529d195041fb20f1ca251413274d8c012f1d7
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-02-26 21:30:01 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(){
+   println( Income(5, 3000))
+}
+fun Income(hours :Int, rate :Int):Int{
+return hours * rate
+
+}
